44eb0569f6e3d4653bd4d8d8abdf63d6ead1d829
lgcirilo <lgcirilo@gmail.com>
2023-09-18 23:40:23 -0700

    Basic auth with bcrypt encoder.

--- src/main/java/com/example/springsecuritytutorial/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritytutorial/security/ApplicationSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.example.springsecuritytutorial.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,13 +18,31 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class ApplicationSecurityConfig {
 
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "index.html", "/css/*", "/js/*")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // internally this is ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
